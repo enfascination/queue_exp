40d3da37d554e55c50e9f53cdc8a785d2840925b
Seth Frey <seth.frey@dartmouth.edu>
2017-03-26 13:47:53 -0400

    tweak to dump

--- dumpData.sh
+++ dumpData.sh
@@ -1,3 +1,4 @@
+echo "i had to port install mongo-tools to get this working"
 mongoexport --jsonArray --pretty -h 127.0.0.1 --port 3001 -d meteor -c questions -o ./data_questions.json
 mongoexport --jsonArray --pretty -h 127.0.0.1 --port 3001 -d meteor -c s_data -o ./data_questions_obj.json
 mongoexport --jsonArray --pretty -h 127.0.0.1 --port 3001 -d meteor -c s_status -o ./data_subjects.json
